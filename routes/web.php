8a0c9c012cf7a1d9549caecf72f42cc4836d24da
letoceiling-coder <dsc-23@yandex.ru>
2026-02-06 21:52:29 +0300

    Fix Swagger: Reorder routes to allow l5-swagger docs endpoint

--- routes/web.php
+++ routes/web.php
@@ -10,13 +10,17 @@ Route::get('/', function () {
     return response($html)->header('Content-Type', 'text/html; charset=utf-8');
 });
 
-// Альтернативный роут для документации
+// Альтернативный роут для документации (только если не запрос к Swagger)
 Route::get('/docs', function () {
+    // Проверяем, не запрашивается ли JSON/YAML файл для Swagger
+    if (request()->has('api-docs.json') || request()->has('api-docs.yaml')) {
+        abort(404); // Пусть l5-swagger обработает
+    }
     $html = File::get(public_path('index_docs.html'));
     return response($html)->header('Content-Type', 'text/html; charset=utf-8');
-});
+})->where('docs', '^docs$');
 
-// Fix for l5-swagger route name issue
+// Fix for l5-swagger route name issue - должен быть ПЕРЕД /docs
 Route::get('/docs/{jsonFile?}', [SwaggerController::class, 'docs'])
     ->name('l5-swagger.default.docs')
     ->where('jsonFile', '.*');
