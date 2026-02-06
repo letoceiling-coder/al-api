55a924be71fa1d31f07b5917dace12f44fe0afc9
letoceiling-coder <dsc-23@yandex.ru>
2026-02-06 20:51:27 +0300

    Fix Swagger: Add route with proper name l5-swagger.default.docs

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\File;
+use L5Swagger\Http\Controllers\SwaggerController;
 
 // Главная страница - документация
 Route::get('/', function () {
@@ -14,3 +15,8 @@ Route::get('/docs', function () {
     $html = File::get(public_path('index_docs.html'));
     return response($html)->header('Content-Type', 'text/html; charset=utf-8');
 });
+
+// Fix for l5-swagger route name issue
+Route::get('/docs/{jsonFile?}', [SwaggerController::class, 'docs'])
+    ->name('l5-swagger.default.docs')
+    ->where('jsonFile', '.*');
