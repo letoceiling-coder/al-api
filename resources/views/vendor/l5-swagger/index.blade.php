3b56dddbe1dfe6396946838cb58e820803a90da1
letoceiling-coder <dsc-23@yandex.ru>
2026-02-06 20:47:47 +0300

    Fix Swagger UI route: Use absolute URL for docs endpoint

--- resources/views/vendor/l5-swagger/index.blade.php
+++ resources/views/vendor/l5-swagger/index.blade.php
@@ -37,7 +37,7 @@
     <script>
         window.onload = function() {
             const ui = SwaggerUIBundle({
-                url: "{{ url('docs?api-docs.json') }}",
+                url: "{{ url('/docs') }}?api-docs.json",
                 dom_id: '#swagger-ui',
                 deepLinking: true,
                 presets: [
