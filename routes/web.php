d5c9bbf4ef7e89f3d3a197d2136eefd8a9fe6c7e
letoceiling-coder <dsc-23@yandex.ru>
2026-02-06 17:55:01 +0300

    Add documentation web page

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,16 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\File;
 
+// Главная страница - документация
 Route::get('/', function () {
-    return view('welcome');
+    $html = File::get(public_path('index_docs.html'));
+    return response($html)->header('Content-Type', 'text/html; charset=utf-8');
+});
+
+// Альтернативный роут для документации
+Route::get('/docs', function () {
+    $html = File::get(public_path('index_docs.html'));
+    return response($html)->header('Content-Type', 'text/html; charset=utf-8');
 });
